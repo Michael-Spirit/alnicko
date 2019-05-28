e5bc4e8bc9fead89387f950f50011994f63aea41
Michael Spirit <MSpiridonov94@gmail.com>
2019-05-29 02:37:48 +0300

    second day playing with profiler

--- large_data/generate_test_files.py
+++ large_data/generate_test_files.py
@@ -1,6 +1,8 @@
 import os
+import time
 import datetime
 import random
+import json
 
 
 # generates test .pos file with ~4.2 Mb size
@@ -28,13 +30,21 @@ if __name__ == '__main__':
         file.write("% (lat/lon/height=WGS84/ellipsoidal,Q=1:fix,2:float,3:sbas,4:dgps,5:single,6:ppp,ns=# of satellites)\n")
         file.write("%  GPST                  latitude(deg) longitude(deg)  height(m)   Q  ns   sdn(m)   sde(m)   sdu(m)  sdne(m)  sdeu(m)  sdun(m) age(s)  ratio\n")
 
-        now = datetime.datetime(2019, 1, 1)
+        now = datetime.datetime.now()
         for _ in range(30000):
             now = now + datetime.timedelta(microseconds=random.randint(500_000, 2_000_000))
             file.write(f"{now.strftime('%Y/%m/%d %H:%M:%S.%f')}   32.041449250   34.816069084    57.1309   1   6   6.2565  18.5049  24.1780   7.4584 -19.6609 -10.9075   0.19    4.3\n")
 
+    now = time.time_ns()
     for x in range(5):
-        if not os.path.exists(os.path.dirname(os.path.abspath(__file__)) + '/' + json_name.format(x)):
-            os.system(f'touch {json_name.format(x)}')
-        else:
-            pass
+        index = 0
+        os.system(f'touch {json_name.format(x)}')
+
+        test_data = {"timestamps": [], "device_alias": "test", "filename": "test.svo", 'beginning': now}
+        for _ in range(30_000):
+            test_data['timestamps'].append([index, now])
+            now += 200_000_000
+        test_data['end'] = now
+        test_data['total'] = len(test_data['timestamps'])
+        with open(json_name.format(x), 'w') as json_file:
+            json.dump(test_data, json_file)
